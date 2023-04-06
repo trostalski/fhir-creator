12a960f245687f6058e758480219872ed5741b42
tillrostalski <rostis@hotmail.de>
2023-04-06 20:59:17 +0200

    added value sets

---
+++ src/data/extract_valuesets.py
@@ -0,0 +1,13 @@
+import json
+
+with open("./fhir-definitions/valuesets.json") as rf:
+    raw = json.load(rf)
+    for entry in raw["entry"]:
+        resource = entry["resource"]
+        file_name = entry["fullUrl"].split("/")[-1]
+        if resource["resourceType"] == "CodeSystem":
+            with open(f"codesystems/{file_name}.json", "w") as wf:
+                json.dump(resource, wf, ensure_ascii=False, indent=4)
+        elif resource["resourceType"] == "ValueSet":
+            with open(f"valuesets/{file_name}.json", "w") as wf:
+                json.dump(resource, wf, ensure_ascii=False, indent=4)
