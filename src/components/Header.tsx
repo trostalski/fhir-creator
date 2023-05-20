35a5e6452a9419b6eb66f16d6e96fc194ecdc5b8
tillrostalski <rostis@hotmail.de>
2023-05-20 22:38:12 +0200

    add head

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <header>
       <nav className="flex flex-row fixed top-0 w-full h-10 bg-blue-400 items-center text-white font-light px-4 z-10">
-        <span> FHIR Creator</span>
+        <h1> FHIR Creator</h1>
         <span className="flex-grow"></span>
         <div className="flex flex-row gap-4">
           <a href="https://github.com/trostalski/fhir-creator" target="_blank">
