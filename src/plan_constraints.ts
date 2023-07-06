c256369b39466a31cf61db2e20267e3e1cce5abb
tillrostalski <rostis@hotmail.de>
2023-07-06 18:38:37 +0200

    replaced methods by existing functions

--- src/plan_constraints.ts
+++
@@ -1,6 +0,0 @@
-// when to check for constraints?
-// at resource creation
-// what triggers constraint check? -> resource creation button
-// check carinalities first
-// create json
-// then check constraints 
