74c1af486cf8f589bb93a45932f0c85b95812548
ItIsGreg <g.nageler@web.de>
2023-09-26 01:11:14 +0200

    style API-key input field

--- src/components/annotator/ApiKeyModal.tsx
+++ src/components/annotator/ApiKeyModal.tsx
@@ -13,21 +13,33 @@ export const ApiKeyModal = (props: ApiKeyModalProps) => {
   };
   return (
     <ModalWrapper setShow={props.setShowApiKeyModal}>
-      <div className="flex flex-row gap-8">
-        <input
-          className="grow"
-          type="text"
-          onChange={(e) => setApiKey(e.target.value)}
-        />
-        <button
-          onClick={async () => {
-            await handleSetApiKey(apiKey);
-            props.setShowApiKeyModal(false);
-          }}
-        >
-          Set Api Key
-        </button>
-        <button onClick={() => props.setShowApiKeyModal(false)}>Cancel</button>
+      <div className="flex flex-col gap-4 items-center">
+        <h2 className="text-xl font-semibold text-center text-blue-600 mb-2">
+          Add your OpenAI - API Key
+        </h2>
+        <div className="flex flex-row gap-4 w-full">
+          <input
+            className="grow"
+            type="text"
+            onChange={(e) => setApiKey(e.target.value)}
+          />
+          <button
+            className="text-gray-100 text-lg font-semibold py-1 rounded-lg hover:bg-blue-700 bg-blue-500 p-2"
+            onClick={async () => {
+              await handleSetApiKey(apiKey);
+              props.setShowApiKeyModal(false);
+            }}
+          >
+            Set Api Key
+          </button>
+          <button
+            onClick={() => props.setShowApiKeyModal(false)}
+            className="text-gray-100 text-lg font-semibold py-1 rounded-lg hover:bg-blue-700 bg-blue-500 p-2"
+          >
+            Cancel
+          </button>
+        </div>
+        <p>Your API Key is stored locally in your browser.</p>
       </div>
     </ModalWrapper>
   );
